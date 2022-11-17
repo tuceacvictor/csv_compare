410c81bf31ebad8d87f65cf4b21a4261be839536
tuceakv <tuceak16@gmail.com>
2022-11-17 20:04:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const generateDeltaFile = (pathToBaseFile, pathToFileForComparison, pathForOutputFileName = './result/difference.csv') => {
+    let baseFileContent = '', secondaryFileContent = '', changedLine = ''
+    return new Promise((resolve, reject) => {
+        const baseFileReadstream = fs.createReadStream(pathToBaseFile)
+        baseFileReadstream
+            .on('data', chunk => { baseFileContent = baseFileContent + chunk })
+            .on('error', (err) => reject(err))
+            .on('end', () => {
+                const secondaryFileReadstream = fs.createReadStream(pathToFileForComparison)
+                secondaryFileReadstream
+                    .on('data', data => { secondaryFileContent = secondaryFileContent + data })
+                    .on('error', (err) => reject(err))
+                    .on('end', () => {
+                        // split all lines by \n to form an array for both base and secondary files
+                        const internLines = baseFileContent.toString().split('\n');
+                        const externLines = secondaryFileContent.toString().split('\n');
+
+                        // Create a json object with each secondary file line as its key and value as true
+                        const externLookup = {};
+                        externLines.forEach(eLine => externLookup[eLine] = true);
+
+                        // Iterate through each line of base file
+                        internLines.forEach(iLine => {
+                            // use above formed json object and pass each line as key
+                            // value of externLookup[iLine] would be undefined if secondary file didn't have same line
+                            // in that case current line is considered as changed line and will be eventually written to output file
+                            if (!externLookup[iLine]) changedLine = changedLine + iLine + '\n'
+                        })
+
+                        fs.writeFileSync(pathForOutputFileName, changedLine)
+                        resolve('DONE')
+                    })
+            })
+    })
+}
+
+generateDeltaFile('./source/fileOne.csv', './source/fileTwo.csv')
